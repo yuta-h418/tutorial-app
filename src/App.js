7d4032fa6c997b3bb7c94e0479d05cfb5ce4ce9f
Yuta Hoshina <yuta.h.8888@gmail.com>
2025-03-04 23:33:16 +0900

    チュートリアル - コンポーネント

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import { HelloWorld } from './components/HelloWorld';
-
-function App() {
+function Profile() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <img
+      src="https://i.imgur.com/lICfvbD.jpg"
+      alt="Aklilu Lemma"
+    />
   );
 }
 
-export default App;
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
