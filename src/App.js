68ea8ac7d60081a7729711ef57133e8ef00d711d
Yuta Hoshina <yuta.h.8888@gmail.com>
2025-03-06 00:20:41 +0900

    チュートリアル - コンポーネントのインポート/エクスポート

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import { HelloWorld } from './components/HelloWorld';
+import Gallery from './Gallery.js';
+import { Profile } from './Profile.js';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Profile />
+      <Gallery />
     </div>
   );
 }
-
-export default App;
