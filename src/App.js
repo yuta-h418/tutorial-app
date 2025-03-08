42f20051eead0373f151de57c71f33729788fdfe
Yuta Hoshina <yuta.h.8888@gmail.com>
2025-03-09 00:43:06 +0900

    チュートリアル

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import { HelloWorld } from './components/HelloWorld';
-
-function App() {
+function Avatar() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <img
+      className="avatar"
+      src="https://i.imgur.com/1bX5QH6.jpg"
+      alt="Lin Lanying"
+      width={100}
+      height={100}
+    />
   );
 }
 
-export default App;
+export default function Profile() {
+  return (
+    <Avatar />
+  );
+}
